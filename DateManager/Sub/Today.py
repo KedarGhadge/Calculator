36a319c1d4dd257f84048fc560e2e197aec4deb0
KedarGhadge <kghadge84@gmail.com>
2019-02-01 00:09:37 +0530

    changed in Today.py

--- DateManager/Sub/Today.py
+++ DateManager/Sub/Today.py
@@ -2,13 +2,17 @@ from datetime import date
 from datetime import time
 from datetime import datetime
 class ToDate:
+	def __init__(self):
+		self.today=date.today()
+		
 	def getToday(self):
 		try:
-			today=date.today()
-			print("Today's date is",today)
+			print("Today's date is",self.today)
 		except Exception as e:
 			# print(e)
 			# print(str(e))
 			# print(e.args)
 			print("Exception occurred in Today exiting a code"+e)
 			exit(1)
+			
+	
