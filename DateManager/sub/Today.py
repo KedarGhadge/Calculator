350340502353b97597f99cef0dbda58e2a95f4ff
KedarGhadge <kghadge84@gmail.com>
2019-02-05 23:54:01 +0530

    added missing file

---
+++ DateManager/sub/Today.py
@@ -0,0 +1,28 @@
+from datetime import date
+from datetime import time
+from datetime import datetime
+class ToDate:
+	def __init__(self):
+		self.today=date.today()
+		self.cTime=datetime.now()
+	def getToday(self):
+		try:
+			return self.today,self.today.strftime("%A")
+			#print("Today's date is",str(self.today) +", "+self.today.strftime("%A"))
+		except Exception as e:
+			# print(e)
+			print(str(e))
+			# print(e.args)
+			#print("Exception occurred in Today exiting a code"+e.tostring())
+			exit(1)
+
+	def getTime(self):
+		try:
+			return self.cTime.strftime("%X")
+			#print("Current time is ",self.cTime.strftime("%X"))
+		except Exception as e:
+			# print(e)
+			print(str(e))
+			# print(e.args)
+			#print("Exception occurred in Today exiting a code"+e.tostring())
+			exit(1)
